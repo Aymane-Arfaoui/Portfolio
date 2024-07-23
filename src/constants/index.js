7615e674d70ea7bfc2084f1a0c6329c15983e1db
Aymane-Arfaoui <arfaouiaymane@hotmail.com>
2024-07-23 00:47:04 -0400

    change of text

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `Hello! My name is Aymane Arfaoui, and I am a third year at Concordia University majoring in Software Engineering. I have previously interned with Pratt & Whitney Canada and Raytheon RTX, where I worked as a Data Analyst and Machine Learning Engineer. In my free time, I work on side projects including a Web-Based University Exchange Platform and an AI Medical Counselor Startup. Please feel free to reach out!`;
+export const HERO_CONTENT = `My name is Aymane Arfaoui, and I am a third year at Concordia University majoring in Software Engineering. I have previously interned with Pratt & Whitney Canada and Raytheon RTX, where I worked as a Data Analyst and Machine Learning Engineer. In my free time, I work on side projects including a Web-Based University Exchange Platform and an AI Medical Counselor Startup. Please feel free to reach out!`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
